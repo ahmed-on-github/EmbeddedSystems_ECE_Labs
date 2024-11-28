e1eaaeb0f63e34d08a060d1576dab3ff6a03e991
ahmed-on-github <159645855+ahmed-on-github@users.noreply.github.com>
2024-11-28 23:16:58 +0200

    Delete GccApplication1 directory

--- GccApplication1/Debug/LED.d
+++
@@ -1,9 +0,0 @@
-LED.d LED.o: .././LED.c .././LED.h .././dio.h .././reg.h .././int_types.h
-
-.././LED.h:
-
-.././dio.h:
-
-.././reg.h:
-
-.././int_types.h:
--- GccApplication1/Debug/dio.d
+++
@@ -1,7 +0,0 @@
-dio.d dio.o: .././dio.c .././dio.h .././reg.h .././int_types.h
-
-.././dio.h:
-
-.././reg.h:
-
-.././int_types.h:
