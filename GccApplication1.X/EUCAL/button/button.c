f7df4af6b6e24626a7383930cbce3527de784fb5
ahmed-on-github <159645855+ahmed-on-github@users.noreply.github.com>
2024-11-28 23:17:32 +0200

    Delete GccApplication1.X directory

--- GccApplication1.X/EUCAL/button/button.c
+++
@@ -1,8 +0,0 @@
-#include "button.h"
-
-void button_init(uint8_t port , uint8_t pin){
-    DIO_init(port,pin,IN);
-}
-uint8_t button_read(uint8_t port , uint8_t pin){
-    return DIO_read(port,pin);
-}
